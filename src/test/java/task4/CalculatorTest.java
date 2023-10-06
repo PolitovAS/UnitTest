aac88b255217d4319ea1aca76ea5c7851619c204
PolitovAS <razreztv@gmail.com>
2023-10-07 01:38:11 +0500

    add seminar_1 code

---
+++ src/test/java/task4/CalculatorTest.java
@@ -0,0 +1,96 @@
+package task4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+class CalculatorTest {
+    Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @BeforeAll
+    public static void initAll() {
+        System.out.printf("Hiii!!!!!");
+    }
+
+    @AfterAll
+    public static void tearDownAll() {
+        System.out.printf("END!");    }
+
+    @AfterEach
+    public void tearDown() {
+        calculator = null; // Очистка после каждого теста
+    }
+
+    @Test
+    @DisplayName("Проверка равенства значений")
+    void testAssertEquals() {
+        assertEquals(4, calculator.add(2, 2));
+    }
+
+    @Test
+    @DisplayName("Проверка неравенства значений")
+    void testAssertNotEquals() {
+        assertNotEquals(5, calculator.add(2, 2));
+    }
+
+    @Test
+    @DisplayName("Проверка истинного условия")
+    void testAssertTrue() {
+        assertTrue(calculator.add(2, 2) == 4);
+    }
+
+    @Test
+    @DisplayName("Проверка ложного условия")
+    void testAssertFalse() {
+        assertFalse(calculator.add(2, 2) == 5);
+    }
+
+    @Test
+    @DisplayName("Проверка наличия объекта")
+    void testAssertNotNull() {
+        assertNotNull(calculator);
+    }
+
+    @Test
+    @DisplayName("Проверка отсутствия объекта")
+    void testAssertNull() {
+        calculator = null;
+        assertNull(calculator);
+    }
+
+    @Test
+    @DisplayName("Проверка равенства массивов")
+    void testAssertArrayEquals() {
+        int[] expected = {1, 2, 3};
+        int[] actual = {1, 2, 3};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Проверка на генерацию исключений")
+    void testAssertThrows() {
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(1, 0));
+    }
+
+    // Дополнительные ассерты
+    @Test
+    @DisplayName("Проверка с сообщением")
+    void testAssertWithMessage() {
+        assertEquals(4, calculator.add(2, 2), "2 + 2 должно быть равно 4");
+    }
+
+    @Test
+    @DisplayName("Проверка с использованием лямбда-выражения для сообщения")
+    void testAssertWithLambdaMessage() {
+        assertEquals(4, calculator.add(2, 2), () -> "2 + 2 должно быть равно 4");
+    }
+
+
+
+
+}
